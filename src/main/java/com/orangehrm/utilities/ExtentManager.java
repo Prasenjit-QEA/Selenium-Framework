72df517b328c0eccf6b63266ae0b47552f84a394
Prasenjit-QEA <prasenjitqea@gmail.com>
2026-02-08 23:19:51 +0530

    Fixing reporting dir for Jenkins

--- src/main/java/com/orangehrm/utilities/ExtentManager.java
+++ src/main/java/com/orangehrm/utilities/ExtentManager.java
@@ -19,136 +19,148 @@ import java.util.Map;
 
 public class ExtentManager {
     private static ExtentReports extent;
-    public static ThreadLocal<ExtentTest> test= new ThreadLocal<>();
+    public static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
     public static Map<Long, WebDriver> driverMap = new HashMap<>();
 
-    //Initialize the Extent Report
-    public static synchronized ExtentReports getReporter(){
-       if (extent==null){
-           String reportPath =System.getProperty("user.dir")+"/src/test/resources/ExtentReport/ExtentReport.html";
-           ExtentSparkReporter spark =new ExtentSparkReporter(reportPath);
-           spark.config().setReportName("Automation Test Report");
-           spark.config().setDocumentTitle("OrangeHRM Report");
-           spark.config().setTheme(Theme.DARK);
+    // Initialize the Extent Report
+    public static synchronized ExtentReports getReporter() {
+        if (extent == null) {
+            // Generate reports in src/test/resources/ExtentReport
+            String reportDir = System.getProperty("user.dir") + "/src/test/resources/ExtentReport";
+            String reportPath = reportDir + "/ExtentReport.html";
 
-           extent = new ExtentReports();
-           extent.attachReporter(spark);
-           //Adding system information
-           extent.setSystemInfo("Operating System", System.getProperty("os.name"));
-           extent.setSystemInfo("Java Version",System.getProperty("java.version"));
-           extent.setSystemInfo("User Name",System.getProperty("user.name"));
+            // Create directory if it doesn't exist
+            File reportFolder = new File(reportDir);
+            if (!reportFolder.exists()) {
+                reportFolder.mkdirs();
+            }
+            ExtentSparkReporter spark = new ExtentSparkReporter(reportPath);
+            spark.config().setReportName("Automation Test Report");
+            spark.config().setDocumentTitle("OrangeHRM Report");
+            spark.config().setTheme(Theme.DARK);
 
-       }
-       return extent;
-    }
-    //Start the Test
-    public static synchronized ExtentTest startTest(String testName){
-       ExtentTest extentTest = getReporter().createTest(testName);
-       test.set(extentTest);
-       return extentTest;
-    }
+            extent = new ExtentReports();
+            extent.attachReporter(spark);
+            // Adding system information
+            extent.setSystemInfo("Operating System", System.getProperty("os.name"));
+            extent.setSystemInfo("Java Version", System.getProperty("java.version"));
+            extent.setSystemInfo("User Name", System.getProperty("user.name"));
 
-    //Get Current Thread's Test
-    public static synchronized ExtentTest getTest(){
-       return test.get();
+        }
+        return extent;
     }
 
-    //Method to get the name of the current test
-    public static String getTestName(){
-       ExtentTest currentTest = getTest();
-       if (currentTest!=null){
-           return currentTest.getModel().getName();
-       }
-       else {
-           return "No test is currently active for this thread";
-       }
+    // Start the Test
+    public static synchronized ExtentTest startTest(String testName) {
+        ExtentTest extentTest = getReporter().createTest(testName);
+        test.set(extentTest);
+        return extentTest;
     }
 
-    //Log a step
-    public static void logStep(String logMessage){
+    // Get Current Thread's Test
+    public static synchronized ExtentTest getTest() {
+        return test.get();
+    }
+
+    // Method to get the name of the current test
+    public static String getTestName() {
+        ExtentTest currentTest = getTest();
+        if (currentTest != null) {
+            return currentTest.getModel().getName();
+        } else {
+            return "No test is currently active for this thread";
+        }
+    }
+
+    // Log a step
+    public static void logStep(String logMessage) {
         getTest().info(logMessage);
     }
 
-    //Log a step validation with screeshot
-    public static void logStepWithScreenshot(WebDriver driver, String logMessage, String Screenshotmessage){
+    // Log a step validation with screeshot
+    public static void logStepWithScreenshot(WebDriver driver, String logMessage, String Screenshotmessage) {
         getTest().pass(logMessage);
-        //Screenshot method
-        attachScreenshot(driver,Screenshotmessage);
+        // Screenshot method
+        attachScreenshot(driver, Screenshotmessage);
     }
 
-    //Log a step validation for API
-    public static void logStepValidationforAPI(String logMessage){
+    // Log a step validation for API
+    public static void logStepValidationforAPI(String logMessage) {
         getTest().pass(logMessage);
     }
 
-    //Log a Failure
-    public static void logFailure(WebDriver driver, String logMessage, String Screenshotmessage){
-        String colorMessage = String.format("<span style='color:red;'>%s</span",logMessage);
+    // Log a Failure
+    public static void logFailure(WebDriver driver, String logMessage, String Screenshotmessage) {
+        String colorMessage = String.format("<span style='color:red;'>%s</span", logMessage);
         getTest().fail(colorMessage);
-        //Screenshot method
-        attachScreenshot(driver,Screenshotmessage);
+        // Screenshot method
+        attachScreenshot(driver, Screenshotmessage);
     }
-    //Log a Failure for API
-    public static void logFailure(String logMessage){
-        String colorMessage = String.format("<span style='color:red;'>%s</span",logMessage);
+
+    // Log a Failure for API
+    public static void logFailure(String logMessage) {
+        String colorMessage = String.format("<span style='color:red;'>%s</span", logMessage);
         getTest().fail(colorMessage);
     }
 
-    //Log a skip
-    public static void logSkip(String logMessage){
-        String colorMessage = String.format("<span style='color:grey;'>%s</span",logMessage);
+    // Log a skip
+    public static void logSkip(String logMessage) {
+        String colorMessage = String.format("<span style='color:grey;'>%s</span", logMessage);
         getTest().skip(colorMessage);
     }
-    public synchronized static void endTest(){
+
+    public synchronized static void endTest() {
         getReporter().flush();
     }
 
-
-    //Take a screenshot with date and time in the file
-    public synchronized static String takeScreenshot(WebDriver driver, String screenshotName){
-        TakesScreenshot ts = (TakesScreenshot)driver;
-        File src=ts.getScreenshotAs(OutputType.FILE);
-        //Format Date and Time for file name
-        String TimeStamp=new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new Date());
-        //Saving the Screenshot to a file
-        String destPath =System.getProperty("user.dir")+"/src/test/resources/screenshots/"+screenshotName+"_"+TimeStamp+".png";
+    // Take a screenshot with date and time in the file
+    public synchronized static String takeScreenshot(WebDriver driver, String screenshotName) {
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File src = ts.getScreenshotAs(OutputType.FILE);
+        // Format Date and Time for file name
+        String TimeStamp = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new Date());
+        // Saving the Screenshot to a file
+        String destPath = System.getProperty("user.dir") + "/src/test/resources/screenshots/" + screenshotName + "_"
+                + TimeStamp + ".png";
 
         File finalPath = new File(destPath);
         try {
-            FileUtils.copyFile(src,finalPath);
+            FileUtils.copyFile(src, finalPath);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        //Convert screenshot to Base64 fir embedding in the Report
-        String base64Format=convertTobase64(src);
+        // Convert screenshot to Base64 fir embedding in the Report
+        String base64Format = convertTobase64(src);
         return base64Format;
     }
 
-    //Convert Screenshot to base64
-    public synchronized static String convertTobase64(File screenShotFile){
-        String base64Format="";
-        //Read the file content into a byte array
+    // Convert Screenshot to base64
+    public synchronized static String convertTobase64(File screenShotFile) {
+        String base64Format = "";
+        // Read the file content into a byte array
         byte[] fileContent = new byte[0];
         try {
             fileContent = FileUtils.readFileToByteArray(screenShotFile);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        //convert the byte to Base64 String
+        // convert the byte to Base64 String
         return base64Format = Base64.getEncoder().encodeToString(fileContent);
     }
 
-    //Attach screenshot to report using Base64
-    public static void attachScreenshot(WebDriver driver, String message){
+    // Attach screenshot to report using Base64
+    public static void attachScreenshot(WebDriver driver, String message) {
         try {
             String screenShotBase64 = takeScreenshot(driver, getTestName());
-            getTest().info(message, com.aventstack.extentreports.MediaEntityBuilder.createScreenCaptureFromBase64String(screenShotBase64).build());
-        }catch (Exception e){
-            getTest().fail("Failed to attch screenshot"+message);
+            getTest().info(message, com.aventstack.extentreports.MediaEntityBuilder
+                    .createScreenCaptureFromBase64String(screenShotBase64).build());
+        } catch (Exception e) {
+            getTest().fail("Failed to attch screenshot" + message);
         }
     }
-    //Register WebDriver for current Thread
-    public static void registerDriver(WebDriver driver){
-        driverMap.put(Thread.currentThread().getId(),driver);
+
+    // Register WebDriver for current Thread
+    public static void registerDriver(WebDriver driver) {
+        driverMap.put(Thread.currentThread().getId(), driver);
     }
 }
